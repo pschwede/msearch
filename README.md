449ed5bd28f1a2a90f2e691867dbe77a36e4b976
Peter Schwede <pschwede@posteo.de>
2019-07-05 23:51:15 +0200

    chg: use GNU parallel and moreutils sponge

---
+++ README.md
@@ -0,0 +1,19 @@
+# msearch
+
+Find user profiles by hashtags
+
+## Usage
+
+```bash
+./search linux
+```
+
+## Performance
+
+Query requires 7 minutes on a quadcore with hyperthreading.
+
+## Debian requirements
+
+```bash
+sudo apt-get install parallel moreutils
+```
--- search.sh
+++ search.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+set -x
+
 hashtag=$1
+
+function run() {
+	url=$1
+	hashtag=$2
+	echo $url >&2
+	curl -s https://$url/explore/$hashtag \
+	| egrep -o $url'/@[^"]+' \
+	| tee -a $hashtag.out.txt
+}
+export -f run
+
+
+trap "sort $hashtag.out.txt | uniq | sort > $hashtag.tmp.txt; mv $hashtag.tmp.txt $hashtag.out.txt" EXIT SIGINT SIGQUIT SIGABRT SIGTERM
+
+
 if [ ! -f hosts.lst ]; then
-	(echo qoto.org;echo ifwo.eu;curl -s https://fediverse.network/mastodon | awk -F'<|>' '/href/ && /<\/td>/ {print $3}') > hosts.lst
+	(
+		echo qoto.org
+		echo ifwo.eu
+		curl -s https://fediverse.network/mastodon \
+			| awk -F'<|>' '/href/ && /<\/td>/ {print $3}'
+	) > hosts.lst
 fi
 
-imax=$(wc -l hosts.lst);i=0
-
-(cat hosts.lst | while read url; do i=$(( i + 1 ));echo $i/$imax $url>&2; curl -s https://$url/explore/$hashtag | egrep -o ''$url'/@[^"]+'; done) | tee -a $hashtag.out.txt
-sort $hashtag.out.txt | uniq | sort $hashtag.tmp.txt
-mv $hashtag.tmp.txt $hashtag.out.txt
+#--timeout 5 --jobs 400% 
+cat hosts.lst | parallel run {} "$hashtag"
