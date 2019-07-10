5e87ebc8cd3e42eb90f42b99b52664d0bdee413a
Peter Schwede <pschwede@posteo.de>
2019-07-10 20:46:55 +0200

    new: option to look either for tags in profiles or in toots

--- search.sh
+++ search.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-set -x
+	#| egrep -o '[^"]+/@[^"]+' \
 
 hashtag=$1
-
-function run() {
-	url=$1
-	hashtag=$2
-	echo $url >&2
-	curl -s https://$url/explore/$hashtag \
-	| egrep -o $url'/@[^"]+' \
-	| tee -a $hashtag.out.txt
-}
-export -f run
-
+comand=$2
+hosts="hosts.lst"
 
 trap "sort $hashtag.out.txt | uniq | sort > $hashtag.tmp.txt; mv $hashtag.tmp.txt $hashtag.out.txt" EXIT SIGINT SIGQUIT SIGABRT SIGTERM
 
 
+function explore() {
+	hashtag=$2
+	url="https://$1/explore/$hashtag"
+	echo $url >&2
+	curl -s "$url" \
+	| sed '-nre/@/s/^.+https:\/\/([^/]+)\/(@[^"<>]+).+$/\2@\1/p' \
+	| tee -a $hashtag.out.txt
+}
+export -f explore
+
+
+function tags() {
+	hashtag=$2
+	url="https://$1/tags/$hashtag.rss"
+	echo $url >&2
+	curl -s "$url" \
+	| sed '-nre/guid/s/^.+https:\/\/([^/]+)\/(@[^/"<>]+).+$/\2@\1/p' \
+	| tee -a $hashtag.out.txt
+}
+export -f tags
+
+
 if [ ! -f hosts.lst ]; then
-	(
-		echo qoto.org
-		echo ifwo.eu
 		curl -s https://fediverse.network/mastodon \
 			| awk -F'<|>' '/href/ && /<\/td>/ {print $3}'
-	) > hosts.lst
+			> "$hosts"
 fi
 
-#--timeout 5 --jobs 400% 
-cat hosts.lst | parallel run {} "$hashtag"
+if [ "$comand" == "" ]; then
+	cat "$hosts" | parallel --timeout=5 --jobs=400% explore {} "$hashtag"
+	cat "$hosts" | parallel --timeout=5 --jobs=400% tags {} "$hashtag"
+else
+	cat "$hosts" | parallel --timeout=5 --jobs=400% $comand {} "$hashtag"
+fi
